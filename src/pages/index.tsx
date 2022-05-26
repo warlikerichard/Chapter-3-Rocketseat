f5eea63d55f0bd73a6dea6928528dd6b92299c9c
Warlike Richard <warlikerichard@hotmail.com>
2022-05-26 12:28:25 -0300

    formated date and better scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import { FiCalendar, FiUser} from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -45,46 +48,62 @@ export default function Home({postsPagination} : HomeProps) {
   }, [])
 
   return next_page ? (
-    <>
+
+    <main className={commonStyles.container}>
       {
         allPages.results.map((post) => {
           return(
-            <main className={styles.container} key={post.uid}>
+            <div className={styles.post} key={post.uid}>
               <h1>{post.data.title}</h1>
               <p>{post.data.subtitle}</p>
               <section>
-                <span><FiCalendar className={styles.icon} size={20}/> {post.first_publication_date}</span> <span><FiUser className={styles.icon} size={20}/> {post.data.author}</span>
+                <span>
+                  <FiCalendar className={commonStyles.icon} size={20}/> {post.first_publication_date}
+                  </span> 
+
+                  <span>
+                    <FiUser className={commonStyles.icon} size={20}/> {post.data.author}
+                  </span>
               </section>
-            </main>
+            </div>
           )
         })
       }
-      <main className={styles.container}>
+      <div className={styles.load}>
         <p onClick={() => handleLoadMore(next_page, setNewPage)}>Carregar mais</p>
-      </main>
-    </>
+      </div>
+    </main>
+
   ) : (
-    <>
+
+    <main className={commonStyles.container}>
       {
         allPages.results.map((post) => {
           return(
-            <main className={styles.container} key={post.uid}>
+            <div className={styles.post} key={post.uid}>
               <h1>{post.data.title}</h1>
               <p>{post.data.subtitle}</p>
               <section>
-                <span><FiCalendar className={styles.icon} size={20}/> {post.first_publication_date}</span> <span><FiUser className={styles.icon} size={20}/> {post.data.author}</span>
+                <span><FiCalendar className={commonStyles.icon} size={20}/>
+                 {post.first_publication_date}
+                </span>
+
+                 <span><FiUser className={commonStyles.icon} size={20}/>
+                  {post.data.author}
+                 </span>
               </section>
-            </main>
+            </div>
           )
         })
       }
-    </>
+    </main>
+
   )
 }
 
 //Tarefa: Formatar a data (Incluindo no handleLoadMore)
 
-export const getStaticProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
     const prismic = getPrismicClient({});
     const postsResponse = await prismic.getByType('post', {
       page: 1,
@@ -94,7 +113,13 @@ export const getStaticProps = async () => {
     const results = postsResponse.results.map(post => {
       return{
         uid: post.uid,
-            first_publication_date: post.first_publication_date,
+            first_publication_date: format(
+              new Date(post.first_publication_date),
+              "dd 'de' MMMM 'de' yyyy",
+              {
+                locale: ptBR
+              }
+            ),
             data: {
               title: post.data.title,
               subtitle: post.data.subtitle,
